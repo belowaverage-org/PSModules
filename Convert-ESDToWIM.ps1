ef9e5ffb81d26021b68768d3af938c9626713519
Dylan Bickerstaff <dylan@belowaverage.org>
2022-04-04 15:14:05 -0400

    Update Convert-ESDToWIM.ps1

--- Convert-ESDToWIM.ps1
+++ Convert-ESDToWIM.ps1
@@ -1,4 +1,10 @@
-function Convert-ESDToWIM {
+function Global:Convert-ESDToWIM {
+    <#
+        .SYNOPSIS
+            Converts ESD files to WIM files (Also lets you change WIM editions).
+        .LINK
+            https://cp-wiki/Convert-ESDToWIM
+    #>
     $esdFile = Read-Host -Prompt "Enter the ESD path"
     Get-WindowsImage -ImagePath $esdFile
     $index = Read-Host -Prompt "Enter index"
@@ -9,7 +15,6 @@ function Convert-ESDToWIM {
     }
     New-Item -ItemType Directory -Path ".\TEMP\"
     Mount-WindowsImage -Path ".\TEMP\" -ImagePath "install.wim" -Index 1 -Optimize -CheckIntegrity
-    
     Get-WindowsEdition -Path ".\TEMP\" -Target
     $edition = Read-Host -Prompt "Enter the edition you would like to switch too"
     Set-WindowsEdition -Path ".\TEMP\" -Edition $edition

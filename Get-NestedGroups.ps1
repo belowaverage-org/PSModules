97d3d0837f83be68a01a8bb105eb3a73cff1a275
Dylan Bickerstaff <dylan@belowaverage.org>
2026-01-21 10:04:40 -0500

    Update Get-NestedGroups.ps1

--- Get-NestedGroups.ps1
+++ Get-NestedGroups.ps1
@@ -1,32 +1,38 @@
-$groups = [System.Collections.Generic.Dictionary[string, int]]::new()
-$parents = [System.Collections.Generic.Stack[string]]::new()
+ function Get-ADNestedGroups($DN) {
+    $groups = [System.Collections.Generic.Dictionary[string, [System.Collections.Generic.List[int]]]]::new()
+    $parents = [System.Collections.Generic.Stack[string]]::new()
 
-$startingPoint = (Get-ADUser -Identity USERNAMEGOESHERE).distinguishedName
-
-function Start-EnumWorker($parent) {
-    $group = Get-ADObject -Properties memberOf -Identity $parent
-    if (-not $groups.ContainsKey($parent)) {
-        $parents.Push($parent)
-        $groups.Add($parent, 0)
-        $group.MemberOf | ForEach-Object {
-            Add-GroupCount
-            Start-EnumWorker $_
+    function Start-EnumWorker($parent) {
+        $group = Get-ADObject -Properties memberOf -Identity $parent
+        if (-not $groups.ContainsKey($parent)) {
+            $parents.Push($parent)
+            $groups.Add($parent, @(0, ($parents.Count)))
+            $group.MemberOf | ForEach-Object {
+                Add-GroupCount
+                Start-EnumWorker $_
+            }
+            $_ = $parents.Pop()
         }
-        $_ = $parents.Pop()
+    }
+
+    function Get-Spaces($count) {
+        for ($i = 1; $i -lt $count; $i++) {
+            "    "
+        }
+    }
+
+    function Add-GroupCount() {
+        $parents | ForEach-Object {
+            $groups[$_][0] += 1
+        }
+    }
+
+    Start-EnumWorker $DN
+
+    $groups.Keys | ForEach-Object {
+        [pscustomobject]@{
+            DN = "$(Get-Spaces $groups[$_][1])$_"
+            SubGroups = $groups[$_][0]
+        }
     }
 }
-
-function Add-GroupCount() {
-    $parents | ForEach-Object {
-        $groups[$_] += 1
-    }
-}
-
-Start-EnumWorker $startingPoint
-
-$groups.Keys | ForEach-Object {
-    [pscustomobject]@{
-        DN = $_
-        SubGroups = $groups[$_]
-    }
-} | Sort-Object -Property SubGroups -Descending
